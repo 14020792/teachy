622f95bc880c5ecd17c9aecaef4b76b81b7c360e
Dat Chan <trantrongdat1@gmail.com>
2016-09-28 19:56:48 +0700

    add features to stats view

--- client/app/scripts/controllers/stats.js
+++ client/app/scripts/controllers/stats.js
@@ -11,6 +11,7 @@ angular.module('clientApp')
   .controller('StatsCtrl', function ($scope, headerService) {
     headerService.change('stats');
     // Lay Json subjects tu dau - phia duoi la sample
+    $scope.selectedInstructor = {id: -1}
     $scope.subjects = {
       "Programming":"1",
       "Software Engineering":"2",
@@ -19,22 +20,33 @@ angular.module('clientApp')
     //Lay Json instructors tu dau - Phia duoi la sample
     $scope.instructors = [{
           name: "Nguyen Hoang Hai",
-          teach: ["1","2","3"]
+          teach: ["1","2","3"],
+          imgUrl: "images/img/01.jpg",
+          id: "0"
         }, {
           name: "Tran Trong Dat",
-          teach: ["1","3"]
+          teach: ["1","3"],
+          imgUrl: "images/img/02.jpg",
+          id: "1"
         }, {
           name: "Nguyen Huu Nhat Minh",
-          teach: ["1","2"]
+          teach: ["1","2"],
+          imgUrl: "images/img/03.jpg",
+          id: "2"
         }, {
           name: "Cao Xuan Hung",
-          teach: ["1","2"]
+          teach: ["1","2"],
+          imgUrl: "images/img/04.jpg",
+          id: "3"
         }, {
           name: "Nguyen Trong Dong",
-          teach: ["3","1"]
+          teach: ["3","1"],
+          imgUrl: "images/img/05.jpg",
+          id: "4"
         }
     ];
-    $scope.isSeletedFilter = function(instructor){
+
+    $scope.isSelectedFilter = function(instructor){
       var res = false;
       for(var i=0; i<instructor.teach.length; i++)
         if (instructor.teach[i] == $scope.selectedSubject)
@@ -45,4 +57,5 @@ angular.module('clientApp')
       //     res = true;
       return res;
     };
+
   });
